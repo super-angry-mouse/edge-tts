9b497c42105062562543d18fdb2760ff02f8854b
super-angry-mouse <super.angry.mouse@gmail.com>
2024-10-04 21:37:37 -0400

    reverted changes

--- src/main/java/me/superangrymouse/edgetts/OutputFormats.java
+++ src/main/java/me/superangrymouse/edgetts/OutputFormats.java
@@ -5,7 +5,8 @@ package me.superangrymouse.edgetts;
  */
 public final class OutputFormats {
     public static final String AUDIO_24KHZ_48KBITRATE_MONO_MP3 = "audio-24khz-48kbitrate-mono-mp3";
-    public static final String RAW_48KHZ_16BIT_MONO_PCM  = "raw-48khz-16bit-mono-pcm";
+    public static final String AUDIO_24KHZ_96KBITRATE_MONO_MP3 = "audio-24khz-96kbitrate-mono-mp3";
+    public static final String WEBM_24KHZ_16BIT_MONO_OPUS = "webm-24khz-16bit-mono-opus";
 
     private OutputFormats() {
         throw new UnsupportedOperationException("can not instantiate util class");
